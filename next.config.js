a340dfd532027bd86196279038015b497716c7dd
Istvan Novak <dotneteer@hotmail.com>
2025-07-06 18:49:59 +0200

    wip: progress with Z80 custom syntax

--- next.config.js
+++ next.config.js
@@ -1,6 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+
+const z80Language = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'z80-assembly.tmLanguage.json'), 'utf8'));
+
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
+  mdxOptions: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+    rehypePrettyCodeOptions: {
+      getHighlighter: (options) => {
+        return require('shiki').getHighlighter({
+          ...options,
+          langs: [
+            {
+              id: 'z80-klive',
+              scopeName: 'source.z80-klive',
+              grammar: z80Language,
+              aliases: ['z80'],
+            },
+          ],
+        });
+      },
+    },
+  },
 })
 
 module.exports = withNextra({
